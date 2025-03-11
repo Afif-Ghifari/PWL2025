9f97be8c8b9292201aa8ea9ad6a5a1a23e59ec43
Afif-Ghifari <aghifari1204@gmail.com>
2025-03-12 05:45:43 +0700

    JS4_prak 2.4

--- Minggu_4/Jobsheet4/app/Http/Controllers/UserController.php
+++ Minggu_4/Jobsheet4/app/Http/Controllers/UserController.php
@@ -9,10 +9,19 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        $user = UserModel::where('level_id', 2)->count();
-        // dd($user);
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager tiga tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ],
+        );
+        $user->save();
         return view('user', ['data' => $user]);
         
+        // $user = UserModel::where('level_id', 2)->count();
+        // dd($user);
         // $user = UserModel::where('username', 'manager9')->firstOrFail();
         // $user = UserModel::findOrFail(1);
         // $user = UserModel::findOr(20, ['username', 'password'], 
