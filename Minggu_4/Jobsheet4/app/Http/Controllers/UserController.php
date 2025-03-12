7bbc4783ecbdc6421d5ee60e01efcf383bf6a052
Afif-Ghifari <aghifari1204@gmail.com>
2025-03-12 08:32:55 +0700

    JS4_prak 2.7

--- Minggu_4/Jobsheet4/app/Http/Controllers/UserController.php
+++ Minggu_4/Jobsheet4/app/Http/Controllers/UserController.php
@@ -10,8 +10,11 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::all();
+        $user = UserModel::with('level')->get();
         return view('user', ['data' => $user]);
+        
+        // dd($user);
+        // $user = UserModel::all();
 
         /*function index(){
             // $user = UserModel::firstOrNew(
