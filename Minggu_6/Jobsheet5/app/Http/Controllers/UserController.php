94dc5fa1e3f6673bfaeba27bb3af23be04a68418
Afif-Ghifari <aghifari1204@gmail.com>
2025-03-24 21:12:29 +0700

    JS5b_prak 4

--- Minggu_6/Jobsheet5/app/Http/Controllers/UserController.php
+++ Minggu_6/Jobsheet5/app/Http/Controllers/UserController.php
@@ -22,7 +22,8 @@ class UserController extends Controller
         ];
 
         $activeMenu = 'user';
-        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+        $level = LevelModel::all();
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
     }
     public function create()
     {
@@ -145,6 +146,9 @@ class UserController extends Controller
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
             ->with('level');
 
+        if ($request->level_id) {
+            $users->where('level_id', $request->level_id);
+        }
         return DataTables::of($users)
             // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex) 
             ->addIndexColumn()
