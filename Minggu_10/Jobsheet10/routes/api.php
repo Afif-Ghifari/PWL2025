9d9658887ccf26673fe10eb88ff48cfe66cb0f4c
Afif-Ghifari <aghifari1204@gmail.com>
2025-04-30 13:48:52 +0700

    JS10_prak 1

--- Minggu_10/Jobsheet10/routes/api.php
+++ Minggu_10/Jobsheet10/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+App\Http\Controllers\Api\LoginController::class;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
