db6b802ac948744de028e0c3d962b73bd73f49a3
Afif-Ghifari <aghifari1204@gmail.com>
2025-04-30 14:55:30 +0700

    JS10_prak 4

--- Minggu_10/Jobsheet10/routes/api.php
+++ Minggu_10/Jobsheet10/routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-App\Http\Controllers\Api\LoginController::class;
+use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LevelController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,6 +18,12 @@ App\Http\Controllers\Api\LoginController::class;
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
 Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+
+Route::get('level', [LevelController::class, 'index']);
+Route::post('level', [LevelController::class, 'store']);
+Route::get('level/{level}', [LevelController::class, 'show']);
+Route::put('level/{level}', [LevelController::class, 'update']);
+Route::delete('/level/{level}', [LevelController::class, 'destroy']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
